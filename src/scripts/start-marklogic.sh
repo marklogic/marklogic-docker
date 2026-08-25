78b41464252f1527e90e8a6d6fd39669b4bae4ed
Romain Winieski <winieski@progress.com>
2026-08-25 16:54:06 +0200

    fix copyright

--- src/scripts/start-marklogic.sh
+++ src/scripts/start-marklogic.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 ###############################################################
 #
-#   Copyright © 2018-2025 Progress Software Corporation and/or its subsidiaries or affiliates. All Rights Reserved.
+#   Copyright (c) 2018-2025 Progress Software Corporation and/or its subsidiaries or affiliates. All Rights Reserved.
 #
 ###############################################################
 #   Initialise and start MarkLogic server
